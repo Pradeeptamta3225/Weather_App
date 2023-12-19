01952e51fd0a50beef70afb52168e024350affc2
Pradeeptamta3225 <Pradeeptamta1650@gmail.com>
2023-12-19 12:44:44 +0530

    learn how to get system location

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
 
+
+function renderWeatherInfo(data){
+let newPara = document.createElement('p');
+
+newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`
+
+document.body.appendChild(newPara);
+}
 async function fetchWeatheDetails() {
 
     try{
@@ -13,6 +21,8 @@ async function fetchWeatheDetails() {
         const data = await response.json();
     
         console.log("weatherAPI =>" ,data);
+
+        renderWeatherInfo(data);
     }
 
     catch(err){
@@ -34,4 +44,23 @@ async function getCustomWeatherDetails() {
          console.log(data);
 
     
+}
+
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else{
+        console.log("No Location Found");
+    }
+}
+
+
+function showPosition(position){
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+
+
+    console.log(lat);
+    console.log(long);
 }
